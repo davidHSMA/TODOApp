aa5ebfc9e634e60b4c0804e14112f2d5b8ca0dfb
nadja4 <148196342+nadja4@users.noreply.github.com>
2023-10-18 17:52:59 +0200

    Try to add another detail to task

--- DotNetCoreSqlDb/Migrations/20230509140701_InitialCreate.Designer.cs
+++ DotNetCoreSqlDb/Migrations/20230509140701_InitialCreate.Designer.cs
@@ -36,6 +36,9 @@ namespace DotNetCoreSqlDb.Migrations
                     b.Property<DateTime>("CreatedDate")
                         .HasColumnType("datetime2");
 
+                    b.Property<string>("UploadFile")
+                        .HasColumnType("nvarchar(max)");
+
                     b.Property<string>("Description")
                         .HasColumnType("nvarchar(max)");
 
